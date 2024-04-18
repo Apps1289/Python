40104e69b9bf2bcd0e27af23a50beb9520652daa
Abhi <appsabhirahul@gmail.com>
2024-04-18 13:43:41 +0530

    while_loop

---
+++ while_loop.py
@@ -0,0 +1,34 @@
+# its turn for a looping 
+# While loop
+
+i=1
+while(i<=10):
+    print(f"{i}. Jai Shree Ram")
+    i+=1
+print("Loop terminated")
+
+# > WAP TO PRINT 1-100 
+i=1
+while(i<=10):
+    print(i,end="\t")
+    i+=1
+print("Done!!")
+
+# WAP TO PRINT A TABLE OF ANY NUMBER
+n= 5
+i=1
+while(i<=10):
+    print(f"{n}*{i}={n*i}")
+    i=i+1
+print("Done!")
+
+# WAP TO PRINT ALL THE ELEMENTS FROM A LIST
+list1=[3,4,5,6,7,1,2,9,5]
+i=0
+while(i < len(list1)):
+    print(list1[i])
+    i+=1
+print("Done!!")
+
+
+
