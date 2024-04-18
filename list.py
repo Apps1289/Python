565fafdb27a7e5436db355ce6cbc139dc3804b06
Abhi <appsabhirahul@gmail.com>
2024-04-18 10:55:21 +0530

    list_methods

--- list.py
+++ list.py
@@ -12,6 +12,17 @@ print(str.count("a"))
 # lets have a glimpse about list
 # lists are ordered and mutable
 
-l1 = [1,2,'abhi']
+l1 = [1,2,10,9]
 # can have any datatypes
 print(l1)
+print(len(l1))
+# l1[1] = "raja"
+print(l1.append(56)) # add element at last
+l1.sort()
+print(l1)
+l1.sort(reverse=True)
+print(l1)
+l1.insert(1,"ahi")
+print(l1)
+
+
