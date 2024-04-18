87085d1753628d785d009f462477722b57e136c4
Abhi <appsabhirahul@gmail.com>
2024-04-18 12:38:10 +0530

    nested_dict_and_its_methods

--- dictionary.py
+++ dictionary.py
@@ -14,3 +14,34 @@ dict1["Surname"] = "Uranw" # can add new key value pairs
 
 print(dict1)
 
+# nested dictionary
+
+info = {
+    "Name" : "abc",
+    "Subject" : {
+        "Math": 90,
+        "Science":85,
+        "Social":80
+    }
+}
+print(info)
+print(info["Subject"])
+print(info["Subject"]["Math"])
+
+# Methods in Dictionary
+# dict.keys() >> prints only key values
+print(info.keys())
+print(list(info.keys())) # typecasting> converting dict into list
+
+# dict.values() >> prints only values
+print(info.values())
+
+#dict.items() > it gives tuples of key value pair
+print(dict1.items())
+
+#dict.get("key_name") > for accesing value same as dict["key_name"] but this way it doesn't give error if key is not available in the dictionary
+#it gives none value
+print(info.get("gender"))
+
+# dict.update("key_value_pair") > for adding new key value pairs
+
