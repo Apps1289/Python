2d6de3c30abd01df62f835791af6c2e53de4990d
Abhi <appsabhirahul@gmail.com>
2024-04-18 11:05:50 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,10 @@
+# let's learn about tuple
+# tuple: immutable and ordered
+
+tup1=(1,2,6,3,'prashna')
+print(tup1)
+print(len(tup1))
+# tup1[1] = 'diwass' >> not allowed in tuple since they are immutable
+print(tup1[3])
+print(tup1.index(3))
+print(tup1.count(6))
