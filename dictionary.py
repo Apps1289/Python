0ea0d09daa608fdf77510b5bf4cdd891a27996a3
Abhi <appsabhirahul@gmail.com>
2024-04-18 11:49:30 +0530

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+# Now, lets crave for dictionary in Python
+# >> Dictionary: unordered and mutable
+# >> comes in {key:value} pair
+
+dict1 = {
+    "Name":"abhi",
+    "Age":19,
+    "Branch":"CSE"
+}
+print(dict1)
+print(dict1["Age"]) # can access values using keys
+dict1["Name"] = "Rahul" # can change the value 
+dict1["Surname"] = "Uranw" # can add new key value pairs
+
+print(dict1)
+
