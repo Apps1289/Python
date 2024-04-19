a5243d0c6a5021763ec37555478c4ce4ae3dde09
Abhi <appsabhirahul@gmail.com>
2024-04-19 12:35:39 +0530

    recursion

--- function.py
+++ function.py
@@ -35,5 +35,33 @@ name_cities(cities)
 
 def access_cities(list1):
     for el in range(len(list1)):
-        print(list1[el],end=" ")
+        print(list1[el],end="\n")
 access_cities(cities)
+
+# PRACTISE QUESTONS
+#> RECURSION
+
+def recur(n):
+    if(n==0):
+        return
+    print(n)
+    recur(n-1)
+recur(5)
+
+#> FACTORIAL
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    else:
+        return n * factorial(n-1)
+print(factorial(5))
+
+# print sum of natural number using recursion
+
+def sum_of_natural(n):
+    if(n==0):
+        return 0
+    else:
+        return n + sum_of_natural(n-1)
+print(sum_of_natural(5))
+
