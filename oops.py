aac8256df804c69781ea886e151d8bbb7d1c516f
Abhi <appsabhirahul@gmail.com>
2024-04-22 01:59:47 +0530

    OOPS_CONCEPT

---
+++ oops.py
@@ -0,0 +1,9 @@
+#> OOPS concept
+#> deals with classes and objects
+
+class Student: #>defining classs
+    name="Abhi"
+s1=Student()  #>creating object
+print(s1)
+#>output: <__main__.Student object at 0x0000027272EF96D0>
+print(s1.name)
