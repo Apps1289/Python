ac989def0fe0f917ce76f1ad8794070e2b73a778
Abhi <appsabhirahul@gmail.com>
2024-04-22 10:19:32 +0530

    init_funct

--- oops.py
+++ oops.py
@@ -2,8 +2,23 @@
 #> deals with classes and objects
 
 class Student: #>defining classs
-    name="Abhi"
-s1=Student()  #>creating object
+    name="Abhi" #> for every student's, name will Abhi only
+    #> if we mention class then every student belongs to same class
+s1=Student()#>creating object
 print(s1)
 #>output: <__main__.Student object at 0x0000027272EF96D0>
-print(s1.name)
+print(s1.name) #> accessing the name of the Student defined in the class
+
+#> if we want to give different name of the student class, we need init function;
+class Person:
+     name="Rahul"
+     def __init__(self,fname):  #> here self argument is the most which calls the object
+        self.name = fname
+        # print("We are inside init function")
+# s2=Person()
+s2=Person("Rajesh")
+print(s2.name)
+# just creating an object of a class, it access the __init__ function
+
+
+        
